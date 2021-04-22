962e491a7032c95d444f6c1b333e9cd3b4062246
sabra <sabra@MacBook-Air-Vladimir.local>
2021-04-22 23:06:10 +0300

    Забыл хедер

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -6,7 +6,7 @@
 /*   By: sabra <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 18:18:21 by sabra             #+#    #+#             */
-/*   Updated: 2021/04/22 22:00:50 by sabra            ###   ########.fr       */
+/*   Updated: 2021/04/22 23:05:51 by sabra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,16 @@ typedef struct s_opt
 	size_t	time_to_sleep;
 	size_t	number_of_time_each_philo_must_eat;
 }		t_opt;
+
+typedef struct	s_ph
+{
+	size_t		number;
+	pthread_t	thread;
+	size_t		time_to_die;
+	size_t		time_to_eat;
+	size_t		time_to_sleep;
+	size_t		n_time_must_eat;
+}		t_ph;
 
 int	ft_atoi(const char *str);
 int	init_args(t_opt *opts, int ac, char **av);
