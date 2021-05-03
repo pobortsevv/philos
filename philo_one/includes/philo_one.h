3d804d393fb20a3fc8e07b2852b9660b9a3c9302
sabra <pobortcevv@gmail.com>
2021-05-03 11:01:34 +0300

    Переосмысление философов

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -6,7 +6,7 @@
 /*   By: sabra <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 18:18:21 by sabra             #+#    #+#             */
-/*   Updated: 2021/05/02 15:33:49 by sabra            ###   ########.fr       */
+/*   Updated: 2021/05/03 10:58:03 by sabra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,22 @@
 # define STDERR 2
 # define DEAD 0
 
+typedef struct	s_all
+{
+	size_t		t_to_die;
+	size_t		t_to_eat;
+	size_t		t_to_sleep;
+	size_t		n_of_philos;
+	pthread_mutex_t *forks;
+
+}		t_all;
+
 typedef struct	s_ph
 {
 	size_t		number;
 	pthread_t	thread;
-	size_t		time_to_die;
-	size_t		time_to_eat;
-	size_t		time_to_sleep;
-	size_t		n_time_must_eat;
-	size_t		number_of_philos;
-	size_t		time_to_die_reserv;
-	pthread_mutex_t *forks;
+	int		right;
+	int		left;
 }		t_ph;
 
 int	ft_atoi(const char *str);
--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -6,98 +6,72 @@
 /*   By: sabra <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 16:48:28 by sabra             #+#    #+#             */
-/*   Updated: 2021/05/02 23:07:54 by sabra            ###   ########.fr       */
+/*   Updated: 2021/05/03 11:01:02 by sabra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
-void	forks_lock(t_ph *philo)
-{
-	if (philo->number == 1)
-	{
-		pthread_mutex_lock(&philo->forks[0]);
-		pthread_mutex_lock(&philo->forks[philo->number_of_philos - 1]);
-	}
-	else
-	{
-		pthread_mutex_lock(&philo->forks[philo->number]);
-		pthread_mutex_lock(&philo->forks[philo->number - 1]);
-	}
-}
+t_all	g_all;
 
-void	forks_unlock(t_ph *philo)
-{
-	if (philo->number == 1)
-	{
-		pthread_mutex_unlock(&philo->forks[0]);
-		pthread_mutex_unlock(&philo->forks[philo->number_of_philos - 1]);
-	}
-	else
-	{
-		pthread_mutex_unlock(&philo->forks[philo->number]);
-		pthread_mutex_unlock(&philo->forks[philo->number - 1]);
-	}
-}
+//int	ph_life(t_ph *philo)
+//{
+	//if (philo->time_to_die && philo->time_to_eat < philo->time_to_die)
+	//{
+		//forks_lock(philo);
+		//printf("%zu has taken a fork\n", philo->number);
+		//printf("%zu has taken a fork\n", philo->number);
+		//printf("%zu is eating\n", philo->number);
+		//usleep(philo->time_to_eat * 1000);
+		//forks_unlock(philo);
+		////philo->time_to_die = philo->time_to_die_reserv;
+		//printf("%zu is sleeping\n", philo->number);
+		//usleep(philo->time_to_sleep * 1000);
+		//printf("%zu is thinking\n", philo->number);
+		//usleep(philo->time_to_sleep * 1000);
+		//philo->time_to_die -= philo->time_to_sleep;
+	//}
+	//else
+		//return (printf("%zu philosopher is dead\n", philo->number) * 0);
+	//return (1);
+//}
 
-int	ph_life(t_ph *philo)
-{
-	if (philo->time_to_die && philo->time_to_eat < philo->time_to_die)
-	{
-		forks_lock(philo);
-		printf("%zu has taken a fork\n", philo->number);
-		printf("%zu has taken a fork\n", philo->number);
-		printf("%zu is eating\n", philo->number);
-		usleep(philo->time_to_eat * 1000);
-		forks_unlock(philo);
-		//philo->time_to_die = philo->time_to_die_reserv;
-		printf("%zu is sleeping\n", philo->number);
-		usleep(philo->time_to_sleep * 1000);
-		printf("%zu is thinking\n", philo->number);
-		usleep(philo->time_to_sleep * 1000);
-		philo->time_to_die -= philo->time_to_sleep;
-	}
-	else
-		return (printf("%zu philosopher is dead\n", philo->number) * 0);
-	return (1);
-}
-
-void	*ph_status(void *arg)
-{
-	t_ph *philo;
-
-	philo = (t_ph *)arg;
-	while (ph_life(philo))
-		;
-	return ((void *)DEAD);
-}
-
-void	ph_start(t_ph *philos, pthread_mutex_t *ar_forks)
-{
-	size_t	len;
-	size_t	i;
-	int	err;
-	
-	len = philos[0].number_of_philos;
-	i = 0;
-	while (i < len)
-	{
-		philos[i].forks = ar_forks;
-		err = pthread_create(&philos[i].thread, NULL, ph_status, &philos[i]);
-		if (err != 0)
-		{
-			write(STDERR, "Невозможно создать поток\n", 24); 
-			return ;
-		}
-		err = pthread_detach(philos[i].thread);
-		if (err != 0)
-		{
-			write(STDERR, "Невозможно присоединить поток\n", 29); 
-			return ;
-		}
-		i++;
-	}
-}
+//void	*ph_status(void *arg)
+//{
+	//t_ph *philo;
+//
+	//philo = (t_ph *)arg;
+	//while (ph_life(philo))
+		//;
+	//return ((void *)DEAD);
+//}
+//
+//void	ph_start(t_ph *philos, pthread_mutex_t *ar_forks)
+//{
+	//size_t	len;
+	//size_t	i;
+	//int	err;
+//	
+	//len = philos[0].number_of_philos;
+	//i = 0;
+	//while (i < len)
+	//{
+		//philos[i].forks = ar_forks;
+		//err = pthread_create(&philos[i].thread, NULL, ph_status, &philos[i]);
+		//if (err != 0)
+		//{
+			//write(STDERR, "Невозможно создать поток\n", 24); 
+			//return ;
+		//}
+		//err = pthread_detach(philos[i].thread);
+		//if (err != 0)
+		//{
+			//write(STDERR, "Невозможно присоединить поток\n", 29); 
+			//return ;
+		//}
+		//i++;
+	//}
+//}
 
 int	main(int ac, char **av)
 {
@@ -107,17 +81,17 @@ int	main(int ac, char **av)
 
 	if (ac < 5 || ac > 6)
 		return (write(STDERR, "Wrong number of arguments\n", 26) - 25);
-	philos = init_args(ac, av);
-	if (!philos)
-		return (write(STDERR, "Wrong arguments\n", 16) - 15);
-	forks = (pthread_mutex_t *)malloc(sizeof(pthread_t) * philos[0].number_of_philos);
-	i = 0;
-	while (i < philos[0].number_of_philos)
-	{
-		//TODO Сделать защиту для инициализации
-		pthread_mutex_init(&forks[i], NULL);
-		i++;
-	}
-	ph_start(philos, forks);
+	//philos = init_args(ac, av);
+	//if (!philos)
+		//return (write(STDERR, "Wrong arguments\n", 16) - 15);
+	//forks = (pthread_mutex_t *)malloc(sizeof(pthread_t) * philos[0].number_of_philos);
+	//i = 0;
+	//while (i < philos[0].number_of_philos)
+	//{
+		////TODO Сделать защиту для инициализации
+		//pthread_mutex_init(&forks[i], NULL);
+		//i++;
+	//}
+	//ph_start(philos, forks);
 	return (0);
 }
